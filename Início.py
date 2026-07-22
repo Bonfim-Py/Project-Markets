5484ecbf44aea862d9df5776133737e387adc94a
Bonfim-Py <jvunbcontato18@gmail.com>
2026-07-22 13:54:13 -0300

    Início_Bibliotecas.py
    Added data import and visualization libraries.

---
+++ Início.py
@@ -0,0 +1,11 @@
+print("BOTAFOGO")
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import statistics as sts
+
+data_frame = "Data_frame_Market.csv"
+df = pd.read_csv(data_frame)
+
+df.head()
